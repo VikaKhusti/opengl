cf11250c9d445d7c83ef67b0b3f0448d82e93321
VikaKhusti <viktorijahusti@gmail.com>
2019-09-19 23:50:42 +0300

    how to draw a point

--- main.cpp
+++ main.cpp
@@ -1,69 +1,29 @@
-
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-void display();
-
-void init()
+void display ( void )
 {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
+glClearColor(0.0,0.0,0.0,0.0);
+glClear(GL_COLOR_BUFFER_BIT);
+glPointSize(5);
+glBegin (GL_POINTS);
+
+glColor3f(0.8, 0.5, 0.0 );
+glVertex3f(0.0, 0.0, 0.0); //orange point in center of window
+
+glEnd ();
+glFlush();
 }
 
-void reshape(int, int);
-
-int main (int argc, char**argv)
+int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGB);
-
-    glutInitWindowPosition(100, 100);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("My Window");
-
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-
-    init();
-
-
-    glutMainLoop();
+glutInit(&argc,argv);
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+glutInitWindowSize(700,500);
+glutInitWindowPosition(100,100);
+glutCreateWindow("My_Window");
+glutDisplayFunc(display);
+glutMainLoop();
+return 0;
 }
-
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-
-    glPointSize(10.0);
-
-    //------- draw-------------
-    glBegin(GL_POINTS);
-
-    glVertex2d(5, 5);
-    glVertex2d(-5, -5);
-
-    glEnd();
-
-    glFlush();
-}
-
-void reshape(int w, int h)
-{
-    glViewport(0,0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    //glOrtho(-10, 10, -10, 10, 10, 10); //last 2 argument ???
-    glOrtho(-5,5, -5,5, 2,12);
-   // gluLookAt( 0,0,5, 0,0,0, 0,1,0 );
-    glMatrixMode(GL_MODELVIEW); //default
-
-
-}
-
-
-
-
-
-
-
