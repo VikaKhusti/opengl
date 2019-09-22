6e0fcd2f9592d8cc55a6598bed9dd71e878d797d
VikaKhusti <viktorijahusti@gmail.com>
2019-09-22 12:16:36 +0300

    A ROTATING SQUARE

--- main.cpp
+++ main.cpp
@@ -1,155 +1,61 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
 #include <GL/glut.h>
 
-float x_pos = -10.0;
 
-void display ( void )
+#define window_width  640
+#define window_height 480
+
+// Main loop
+void main_loop_function()
 {
-glClearColor(0.0,0.0,0.0,0.0);
-glClear(GL_COLOR_BUFFER_BIT);
-//glPointSize(5);
-////------------points-------
-//glBegin (GL_POINTS);
-//
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(0.0, 0.0, 0.0); //orange point in center of the window
-//
-//GLfloat y = 0.98;
-//
-//for(int i=0; i < 100; i++)
-//{
-// glColor3f(0.8, 0.5, 0.0 ); //drawing a line from points
-// glVertex3f(0.98, y, 0.0);
-// y -= 0.02;
-//}
-//
-//glEnd ();
-////------------lines-------
-////glPointSize(5); dont work for lines
-//glLineWidth(5.0);
-//glBegin(GL_LINES);
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(-0.98, 0.98, 0.0);
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(-0.98, -0.98, 0.0);
-//
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(-0.98, -0.98, 0.0);
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(0.98, -0.98, 0.0);
-//
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(-0.98, 0.98, 0.0);
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(0.98, 0.98, 0.0);
-//
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(-0.98, 0.0, 0.0);
-//glColor3f(0.8, 0.5, 0.0 );
-//glVertex3f(0.98, 0.0, 0.0);
-//
-//glEnd();
-//
-////------------triangle-----
-//glBegin(GL_TRIANGLES);
-//glColor3f(0.5, 0.5, 0.0 );
-//glVertex3f(-0.8, -0.3, 0.0);
-//glVertex3f(-0.6, -0.8, 0.0);
-//glVertex3f(-0.4, -0.3, 0.0);
-//
-//glColor3f(0.5, 0.5, 0.0 );
-//glVertex3f(-0.6, 0.8, 0.0);
-//glVertex3f(-0.8, 0.3, 0.0);
-//glVertex3f(-0.4, 0.3, 0.0);
-//glEnd();
-//
-////-----------quads----
-//glBegin(GL_QUADS);
-//      glColor3f (0.5, 0.3, 0.6);
-//
-//      glVertex3f (-0.2, 0.5, 0.0);
-//      glVertex3f ( 0.1, 0.5, 0.0);
-//      glVertex3f ( 0.1, 0.2, 0.0);
-//      glVertex3f (-0.2, 0.2, 0.0);
-//
-//    glEnd();
-//
-// glColor3f(1.0, 0.0, 0.0);
-//    glBegin(GL_POLYGON);
-//        glVertex3f(0.2, 0.3, 0.0);
-//        glVertex3f(0.4 , 0.6, 0.0);
-//        glVertex3f(0.9 , 0.3, 0.0);
-//        glVertex3f(0.7 , 0.6, 0.0);
-//    glEnd();
-//
-//glLineWidth(5.0);
-//glBegin(GL_LINES);
-//
-//    glVertex3f(0.2, -0.3, 0.0);
-//    glVertex3f(0.9, -0.3, 0.0);
-//
-//    glVertex3f(0.2, -0.3, 0.0);
-//    glVertex3f(0.4, -0.6, 0.0);
-//
-//    glVertex3f(0.4, -0.6, 0.0);
-//    glVertex3f(0.7, -0.6, 0.0);
-//
-//    glVertex3f(0.7, -0.6, 0.0);
-//    glVertex3f(0.9, -0.3, 0.0);
-//
-//    glColor3f (0.5, 0.3, 0.6);
-//
-//      glVertex3f (-0.2, -0.5, 0.0);
-//      glVertex3f ( 0.1, -0.5, 0.0);
-//
-//      glVertex3f (-0.2, -0.5, 0.0);
-//      glVertex3f ( 0.1, -0.2, 0.0);
-//
-//      glVertex3f ( 0.1, -0.5, 0.0);
-//      glVertex3f (-0.2, -0.2, 0.0);
-//
-//      glVertex3f ( 0.1, -0.2, 0.0);
-//      glVertex3f (-0.2, -0.2, 0.0);
-      //glVertex3f ( 0.1, -0.2, 0.0);
-      //glVertex3f ( 0.1, 0.2, 0.0);
-      //glVertex3f (-0.2, 0.2, 0.0);
-
-
-
-//glEnd();
-//
-//glBegin(GL_LINE_LOOP);
-//        glColor3f(1.0, 1.0, 1.0);
-//        glVertex2f(-0.6, 0.6);
-//        glColor3f(0.0, 0.0, 1.0);
-//        glVertex2f(-0.2, 1.0);
-//        glColor3f(0.0, 1.0, 0.0);
-//        glVertex2f(-0.4, 1.0);
-//        glColor3f(1.0, 0.0, 0.0);
-//        glVertex2f(0.6, 0.6);
-//        glEnd();
-
-glBegin(GL_POLYGON);
- glColor3f (0.5, 0.3, 0.6);
-glVertex2f(x_pos, 1.0);
-glVertex2f(x_pos, -1.0);
-glVertex2f(x_pos + 2.0, -1.0);
-glVertex2f(x_pos + 2.0, 1.0);
-glEnd();
-
-
-glutSwapBuffers();
+   // Z angle
+   static float angle;
+   // Clear color (screen)
+   // And depth (used internally to block obstructed objects)
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   // Load identity matrix
+   glLoadIdentity();
+   // Multiply in translation matrix
+   glTranslatef(0,0, -10);
+   // Multiply in rotation matrix
+   glRotatef(angle, 0, 0, 1);
+   // Render colored quad
+   glBegin(GL_QUADS);
+   glColor3ub(255, 000, 000); glVertex2f(-1,  1);
+   glColor3ub(000, 255, 000); glVertex2f( 1,  1);
+   glColor3ub(000, 000, 255); glVertex2f( 1, -1);
+   glColor3ub(255, 255, 000); glVertex2f(-1, -1);
+   glEnd();
+   // Swap buffers (color buffers, makes previous render visible)
+	glutSwapBuffers();
+   // Increase angle to rotate
+   angle+=0.25;
 }
 
-int main(int argc, char **argv)
+// Initialze OpenGL perspective matrix
+void GL_Setup(int width, int height)
 {
-glutInit(&argc,argv);
-glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
-glutInitWindowSize(700,500);
-glutInitWindowPosition(100,100);
-glutCreateWindow("My_Window");
-glutDisplayFunc(display);
-glutMainLoop();
-return 0;
+
+	glViewport( 0, 0, width, height );
+	glMatrixMode( GL_PROJECTION );
+	glEnable( GL_DEPTH_TEST );
+	gluPerspective( 45, (float)width/height, .1, 100 );
+	glMatrixMode( GL_MODELVIEW );
+}
+
+
+// Initialize GLUT and start main loop
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+
+	glutInitWindowSize(window_width, window_height);
+
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+
+	glutCreateWindow("GLUT Example!!!");
+
+	glutIdleFunc(main_loop_function);
+
+	GL_Setup(window_width, window_height);
+   glutMainLoop();
+
 }
